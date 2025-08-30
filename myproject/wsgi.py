a826818e711448cfadc00f1e3eb1f560ba7c478a
shivam1279 <shivammi035@gmail.com>
2025-08-30 17:59:02 +0530

    1

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -3,7 +3,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.environ.get('STATIC_ROOT', ''))
